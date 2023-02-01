34d7d2332c5ffe82a41590b6e0354fd77c1a04b7
Alex Chaus <chausme@gmail.com>
2023-02-01 10:51:47 +0400

    Add TS watch script

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 export type CallbackType = (...args: any) => any;
 export declare const EventBus: {
     new (): {
-        "__#1@#listeners": Record<string, CallbackType[]>;
+        "__#21743@#listeners": Record<string, CallbackType[]>;
         on(event: string, callback: CallbackType): void;
         off(event: string, callback: CallbackType): void;
         emit(event: string, ...args: any): void;
