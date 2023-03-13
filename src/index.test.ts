824ea29240821bd627e43fa6dd3b2db5c47b78e2
Alex Chaus <chausme@gmail.com>
2023-03-13 12:01:34 +0400

    Add tests Round 3

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,11 @@
 import { EventBus } from './index';
 
 describe('Component', () => {
+    type StateType = Record<string, string | null>;
+
     test('should subscribe for a provided event', () => {
         const testBus = new EventBus();
-        const state: Record<string, string | null> = {
+        const state: StateType = {
             status: null,
         };
         testBus.on('init', () => {
@@ -11,5 +13,23 @@ describe('Component', () => {
         });
         testBus.emit('init');
         expect(state.status).toBe('init');
+    });
+
+    test('should unsubscribe from a provided event', () => {
+        const testBus = new EventBus();
+        const state: StateType = {
+            status: null,
+        };
+        const callback = () => {
+            state.status = 'init';
+        };
+        testBus.on('init', callback);
+        testBus.on('init', () => {
+            state.updated = 'updated';
+        });
+        testBus.off('init', callback);
+        testBus.emit('init');
+        expect(state.status).toBe(null);
+        expect(state.updated).toBe('updated');
     });
 });
